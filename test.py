fe53c09f4ad898bf8a68d0e9aa7ce324bf6cdcfc
tacchan33 <39903348+tacchan33@users.noreply.github.com>
2020-01-08 00:11:30 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,46 @@
+# -*- coding:utf-8 -*-
+
+import paramiko
+import time
+
+host_name = ''
+host_ipaddr = ''
+ssh_username = ''
+ssh_password = ''
+enable_password = ''
+
+client = paramiko.SSHClient()
+client.load_system_host_keys()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+try:
+    client.connect(host_ipaddr, username=ssh_username, password=ssh_password, look_for_keys=False, allow_agent=False)
+    ssh_con = client.invoke_shell()
+    time.sleep(2)
+
+    result = ssh_con.recv(65535).decode('utf-8') #ログイン後の出力メッセージ受け取り
+    print(result)
+    if host_name in result: #ホスト名が一致しているか確認
+        ssh_con.send('enable\n')
+        time.sleep(1)
+        ssh_con.send(enable_password + '\n')
+        time.sleep(1)
+        ssh_con.send('terminal length 0\n')
+        time.sleep(1)
+        print(ssh_con.recv(65535).decode('utf-8'))
+        ssh_con.send('show running-config\n')
+        time.sleep(1)
+        print(ssh_con.recv(65535).decode('utf-8'))
+    else:
+        print('NG')
+
+except Exception as e:
+    
+    print(e)
+    print(host_ipaddr + 'ssh error')
+
+finally:
+    client.close()
+#stdin, stdout, stderr = client.exec_command('')
+#for line in stdout:
+#    print(line)
