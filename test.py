5e9b422dbed1e3346c27a2f8f7aa67d1ea540adc
tacchan33 <39903348+tacchan33@users.noreply.github.com>
2020-01-27 01:13:25 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,48 +1,63 @@
 # -*- coding:utf-8 -*-
 
+import csv
 import paramiko
 import time
 
-hostname = ''
-ssh_username = ''
-ssh_password = ''
-enable_password = ''
+def main():
+    with open('./hosts.csv', newline='') as hosts:
+        csvReader = csv.reader(hosts)
+        for row in csvReader:
+            hostname = row[0]
+            ssh_username = row[1]
+            ssh_password = row[2]
+            enable_password = row[3]
+            result = getResult(hostname,ssh_username,ssh_password,enable_password)
+            try:
+                file = open("./"+hostname+".log","w")
+                file.write(result)
+                print("["+hostname+"] Getting config complete!")
+            except Exception as e:
+                    print("["+hostname+"] Getting config failed")
+                    print(e)
+            finally:
+                file.close()
 
-client = paramiko.SSHClient()
-client.load_system_host_keys()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+def getResult(hostname="Router", ssh_username="admin", ssh_password="password", enable_password="enable"):
 
-try:
-    client.connect(hostname, username=ssh_username, password=ssh_password, look_for_keys=False, allow_agent=False)
-    ssh_connection = client.invoke_shell()
-    time.sleep(2)
-
-    ssh_connection.send('enable\n')
-    time.sleep(1)
-    ssh_connection.send(enable_password + '\n')
-    time.sleep(1)
-    ssh_connection.send('terminal length 0\n')
-    time.sleep(1)
-    ssh_connection.send('copy running-config startup-config\n')
-    time.sleep(1)
-    ssh_connection.send('\n')
-    time.sleep(1)
-    print(ssh_connection.recv(65535).decode('utf-8'))
-    ssh_connection.send('show running-config\n')
-    time.sleep(1)
+    ssh_client = paramiko.SSHClient()
+    ssh_client.load_system_host_keys()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     try:
-        file = open("./"+hostname+".log","w")
-        file.write(ssh_connection.recv(65535).decode('utf-8'))
-        print("Getting config complete!")
-    except Exception as fe:
-        print("Getting config failed")
-        print(fe)
+        ssh_client.connect(hostname, username=ssh_username, password=ssh_password, look_for_keys=False, allow_agent=False)
+        ssh_connection = ssh_client.invoke_shell()
+        time.sleep(2)
 
-except Exception as e:
-    
-    print(e)
-    print(hostname + 'ssh error')
+        ssh_connection.send('enable\n')
+        time.sleep(1)
+        ssh_connection.send(enable_password + '\n')
+        time.sleep(1)
+        ssh_connection.send('terminal length 0\n')
+        time.sleep(1)
+        ssh_connection.send('copy running-config startup-config\n')
+        time.sleep(1)
+        ssh_connection.send('\n')
+        time.sleep(2)
+        print(ssh_connection.recv(65535).decode('utf-8'))
+        ssh_connection.send('show running-config\n')
+        time.sleep(1)
+        
+        return ssh_connection.recv(65535).decode('utf-8')
 
-finally:
-    client.close()
+    except Exception as e:
+        
+        print(e)
+        print(hostname + 'ssh error')
+
+    finally:
+        ssh_client.close()
+
+
+if __name__ == "__main__":
+    main()
