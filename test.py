ae63b352792b3847f05f3e26899971f27090d36c
tacchan33 <39903348+tacchan33@users.noreply.github.com>
2020-01-27 00:39:54 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,59 +3,46 @@
 import paramiko
 import time
 
-host_name = ''
-host_ipaddr = ''
+hostname = ''
 ssh_username = ''
 ssh_password = ''
 enable_password = ''
-
-ftp_hostname = ''
-ftp_dirpath = ''
-ftp_username = ''
-ftp_password = ''
 
 client = paramiko.SSHClient()
 client.load_system_host_keys()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 try:
-    client.connect(host_ipaddr, username=ssh_username, password=ssh_password, look_for_keys=False, allow_agent=False)
-    ssh_con = client.invoke_shell()
+    client.connect(hostname, username=ssh_username, password=ssh_password, look_for_keys=False, allow_agent=False)
+    ssh_connection = client.invoke_shell()
     time.sleep(2)
 
-    result = ssh_con.recv(65535).decode('utf-8') #ログイン後の出力メッセージ受け取り
-    print(result)
-    if host_name in result: #ホスト名が一致しているか確認
-        ssh_con.send('enable\n')
-        time.sleep(1)
-        ssh_con.send(enable_password + '\n')
-        time.sleep(1)
-        ssh_con.send('terminal length 0\n')
-        time.sleep(1)
-        print(ssh_con.recv(65535).decode('utf-8'))
-        ssh_con.send('copy running-config startup-config\n')
-        time.sleep(1)
-        ssh_con.send('\n')
-        time.sleep(1)
-        print(ssh_con.recv(65535).decode('utf-8'))
-        ssh_con.send('copy running-config ftp://' + ftp_username + ':' + ftp_password + '@' + ftp_hostname + ftp_dirpath + '\n')
-        time.sleep(1)
-        ssh_con.send('\n')
-        time.sleep(1)
-        ssh_con.send('\n')
-        time.sleep(1)
-        print(ssh_con.recv(65535).decode('utf-8'))
-        ssh_con.send('show running-config\n')
-        time.sleep(1)
-        print(ssh_con.recv(65535).decode('utf-8'))
+    ssh_connection.send('enable\n')
+    time.sleep(1)
+    ssh_connection.send(enable_password + '\n')
+    time.sleep(1)
+    ssh_connection.send('terminal length 0\n')
+    time.sleep(1)
+    ssh_connection.send('copy running-config startup-config\n')
+    time.sleep(1)
+    ssh_connection.send('\n')
+    time.sleep(1)
+    print(ssh_connection.recv(65535).decode('utf-8'))
+    ssh_connection.send('show running-config\n')
+    time.sleep(1)
 
-    else:
-        print('NG')
+    try:
+        file = open("./"+hostname+".log","w")
+        file.write(ssh_connection.recv(65535).decode('utf-8'))
+        print("Getting config complete!")
+    except Exception as fe:
+        print("Getting config failed")
+        print(fe)
 
 except Exception as e:
     
     print(e)
-    print(host_ipaddr + 'ssh error')
+    print(hostname + 'ssh error')
 
 finally:
     client.close()
